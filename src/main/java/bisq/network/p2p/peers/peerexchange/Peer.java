7344fb16cab72c18f28877c3199ff405a162d386
Manfred Karrer <mk@nucleo.io>
2018-06-27 07:36:53 +0200

    Fix merge issues
    - Add missing exception
    - Add missing parameters
    - Cleanup

--- src/main/java/bisq/network/p2p/peers/peerexchange/Peer.java
+++ src/main/java/bisq/network/p2p/peers/peerexchange/Peer.java
@@ -37,7 +37,7 @@ import lombok.extern.slf4j.Slf4j;
 import javax.annotation.Nullable;
 
 @Getter
-@EqualsAndHashCode(exclude = {"date", "failedConnectionAttempts"})
+@EqualsAndHashCode(exclude = {"date"}) // failedConnectionAttempts is transient and therefore excluded anyway
 @Slf4j
 public final class Peer implements NetworkPayload, PersistablePayload, SupportedCapabilitiesListener {
     private static final int MAX_FAILED_CONNECTION_ATTEMPTS = 5;
